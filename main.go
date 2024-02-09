1d016dd0b033f05e5f11671ac24fef576e78bb04
tikhonp <tikhon.petrishchev@gmail.com>
2024-02-09 15:55:30 +0300

    :sparkles: Added new query event handler object

--- main.go
+++ main.go
@@ -32,9 +32,27 @@ func (record Record) save() error {
 	return err
 }
 
-func NewRecord(data HKRecord) Record {
+func NewRecord(data *HKRecord) Record {
 	log.Println("NewRecord form", data)
 	return Record{CreatedAt: time.Now().UTC(), Time: data.GetTime().UTC(), CategoryName: data.CategoryName, Source: data.Source, Value: data.Value}
+}
+
+type QueryHandleEventJSON struct {
+	CategoryName string `json:"category_name"`
+	ValuesCount  int    `json:"values_count"`
+}
+
+func (e QueryHandleEventJSON) save() error {
+	log.Println("Saving query handle event", e)
+	_, err := db.Exec(`INSERT INTO query_handle_event (created_at, category_name, values_count) VALUES (?, ?, ?)`, time.Now().UTC(), e.CategoryName, e.ValuesCount)
+	return err
+}
+
+type QueryHandleEvent struct {
+	Id           int
+	CreatedAt    time.Time
+	CategoryName string
+	ValuesCount  int
 }
 
 var schema = `
@@ -45,7 +63,15 @@ var schema = `
                 category_name STRING NOT NULL,
                 source STRING NOT NULL,
                 value STRING NOT NULL
-            );`
+            );
+			
+			CREATE TABLE IF NOT EXISTS query_handle_event (
+				id INTEGER PRIMARY KEY NOT NULL,
+				created_at DATETIME NOT NULL,
+				category_name STRING NOT NULL,
+				values_count INTEGER NOT NULL
+			)
+`
 
 func initializeDB() {
 	var sqlErr error
@@ -76,19 +102,31 @@ func (r *HKRecord) GetTime() time.Time {
 
 // URLs handlers
 
+func hkRequestEventHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	var event QueryHandleEventJSON
+	err := json.NewDecoder(r.Body).Decode(&event)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	dbErr := event.save()
+	if dbErr != nil {
+		log.Println("Failed to save query handle event", err.Error())
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
+
 // Handles request with some string data in body and stores it to database
 func hkRequestHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			log.Println("Failed to close request body after read.", err.Error())
-		}
-	}(r.Body)
-
 	dec := json.NewDecoder(r.Body)
 	for {
 		var records []HKRecord
@@ -105,7 +143,7 @@ func hkRequestHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		for _, record := range records {
-			saveErr := NewRecord(record).save()
+			saveErr := NewRecord(&record).save()
 			if saveErr != nil {
 				log.Println("Failed to save new record.", saveErr.Error())
 				w.WriteHeader(http.StatusInternalServerError)
@@ -156,11 +194,40 @@ func getSamplesHandler(w http.ResponseWriter, _ *http.Request) {
 		log.Println("Row iteration error. ", rowsErr.Error())
 	}
 
+	eventsRows, eventQueryErr := db.Query(`SELECT id, created_at, category_name, values_count FROM query_handle_event ORDER BY created_at DESC`)
+	if eventQueryErr != nil {
+		log.Println("Failed to fetch query handle events from db. ", queryErr.Error())
+	}
+	defer func(eventsRows *sql.Rows) {
+		err := eventsRows.Close()
+		if err != nil {
+			log.Println("Failed to close SQL query. ", err.Error())
+		}
+	}(eventsRows)
+
+	var queryHandleEvents []QueryHandleEvent
+	for eventsRows.Next() {
+		var e QueryHandleEvent
+		scanErr := eventsRows.Scan(&e.Id, &e.CreatedAt, &e.CategoryName, &e.ValuesCount)
+		if scanErr != nil {
+			log.Println("Failed to scan event. ", scanErr.Error())
+		}
+		queryHandleEvents = append(queryHandleEvents, e)
+	}
+	if eventRowsErr := eventsRows.Err(); eventRowsErr != nil {
+		log.Println("Row iteration error. ", eventRowsErr.Error())
+	}
+
+	type HtmlData struct {
+		Records           []Record
+		QueryHandleEvents []QueryHandleEvent
+	}
+
 	tmpl, templateErr := template.ParseFiles("records.html")
 	if templateErr != nil {
 		log.Fatal("Failed to load template. ", templateErr.Error())
 	}
-	executeErr := tmpl.Execute(w, records)
+	executeErr := tmpl.Execute(w, HtmlData{Records: records, QueryHandleEvents: queryHandleEvents})
 	if executeErr != nil {
 		log.Println("Execute query failed. ", executeErr.Error())
 		return
@@ -182,6 +249,7 @@ func main() {
 	http.HandleFunc("/", logging(homeHandler))
 	http.HandleFunc("/samples/", logging(getSamplesHandler))
 	http.HandleFunc("/sample/", logging(hkRequestHandler))
+	http.HandleFunc("/query_handle_event/", logging(hkRequestEventHandler))
 
 	httpPort := os.Getenv("PORT")
 	if httpPort == "" {
